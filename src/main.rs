ea60e950f11dc80d2286c93532e39aae4f887e9e
Tim Ryan <id@timryan.org>
2018-05-07 21:03:18 -0400

    More testing.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 
 extern crate libc;
 extern crate pancurses;
+extern crate ncurses;
 extern crate crossbeam_channel;
+extern crate vte;
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
 use crossbeam_channel::unbounded;
-use pancurses::{ALL_MOUSE_EVENTS, endwin, getmouse, cbreak, initscr, mousemask, half_delay, Input, noecho, newpad, Window};
+use pancurses::{ALL_MOUSE_EVENTS, A_BOLD, A_COLOR, COLOR_BLACK, COLOR_RED, A_NORMAL, endwin, getmouse, cbreak, init_pair, initscr, mousemask, half_delay, Input, noecho, newpad, Window};
 
 struct DisplayPad {
     pad: Window,
@@ -17,6 +19,7 @@ struct DisplayPad {
     y: i32,
     width: i32,
     height: i32,
+    vte_parser: Option<vte::Parser>,
 }
 
 impl DisplayPad {
@@ -28,7 +31,9 @@ impl DisplayPad {
             y: 0,
             width,
             height,
+            vte_parser: Some(vte::Parser::new()),
         };
+        init_pair(0, COLOR_BLACK, COLOR_RED);
         pad.clear();
         pad
     }
@@ -37,30 +42,90 @@ impl DisplayPad {
         self.pad.bkgd(' ');
     }
 
-    fn append(&mut self, value: &[u8]) {
+    fn append(&mut self, value: &str) {
         if self.y > self.height {
             // ignore
             return;
         }
-        for c in value {
-            match *c {
-                b'\n' => {
-                    self.y += 1;
-                    self.x = 1;
-                }
-                b'\r' => {
-                    self.x = 1;
-                }
-                c => {
-                    self.pad.mvinsch(self.y, self.x, c as char);
-                    self.x += 1;
-                    if self.x >= self.width {
-                        self.x = 1;
-                        self.y += 1;
-                    }
-                }
+
+        // eprint!("---#> {:?} ", value.len());
+        let mut parser = self.vte_parser.take().unwrap();
+        for b in value.bytes() {
+            parser.advance(self, b);
+        }
+
+        self.vte_parser = Some(parser)
+    }
+}
+
+impl vte::Perform for DisplayPad {
+    fn print(&mut self, c: char) {
+        let mut a = String::new();
+        a.push(c);
+        // eprint!("{:?} ", a);
+        self.pad.printw(&a);
+    }
+
+    fn execute(&mut self, byte: u8) {
+        match byte {
+            b'\n' | b'\r' | 0x8 => {
+                let mut a = String::new();
+                a.push(byte as char);
+                self.pad.printw(&a);
+            }
+            _ => {
+                eprint!("#E{}# ", byte);
             }
         }
+    }
+    
+    fn hook(&mut self, params: &[i64], intermediates: &[u8], ignore: bool) {
+        eprint!("#H# ");
+    }
+    
+    fn put(&mut self, byte: u8) {
+        // let mut a = String::new();
+        // self.pad.printw(&a);
+        eprint!("#P# ");
+    }
+    
+    fn unhook(&mut self) {
+        eprint!("#U# ");
+    }
+
+    fn osc_dispatch(&mut self, params: &[&[u8]]) {
+        eprint!("#O# ");
+    }
+    
+    fn csi_dispatch(
+        &mut self, 
+        params: &[i64], 
+        intermediates: &[u8], 
+        ignore: bool, 
+        c: char
+    ) { 
+        // eprint!("#C {} {:?} {:?}# ", c, params, intermediates);
+        if c == 'm' {
+            match params[0] {
+                0 => {
+                }
+                32 => {
+                    // self.pad.chgat(-1, A_BOLD, 0);
+                    // self.pad.attron(A_BOLD);
+                }
+                _ => {},
+            }
+        }
+    }
+
+    fn esc_dispatch(
+        &mut self, 
+        params: &[i64], 
+        intermediates: &[u8], 
+        ignore: bool, 
+        byte: u8
+    ) { 
+        eprint!("#S# ");
     }
 }
 
@@ -109,13 +174,13 @@ fn main() {
         ret
     };
 
-    let (tx, rx) = unbounded::<u8>();
+    let (tx, rx) = unbounded::<String>();
     ::std::thread::spawn(move || {
         let buf = ::std::io::BufReader::new(&mut stdin);
         // println!("start\r");
-        for c in buf.bytes() {
+        for c in buf.lines() {
             if let Ok(c) = c {
-                tx.send(c);
+                tx.send(format!("{}", c));
             }
         }
     });
@@ -128,7 +193,7 @@ fn main() {
     // pad.append(&contents);
     window.nodelay(true);
     loop {
-        pad.pad.prefresh(scroll_pos,1,0,0,max_y-1,max_x);
+        pad.pad.prefresh(scroll_pos,0,0,0,max_y-1,max_x);
         
         match window.getch() {
             Some(Input::KeyMouse) => {
@@ -158,7 +223,8 @@ fn main() {
             // }
             None => {
                 if let Ok(c) = rx.try_recv() {
-                    pad.append(&[c]);
+                    pad.append(&c);
+                    pad.append("\n");
                     window.refresh();
                 } else {
                     ::std::thread::sleep(::std::time::Duration::from_millis(10));
